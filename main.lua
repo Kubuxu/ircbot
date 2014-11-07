b3b9a45dc5da5bb6f094fed81ba054f6b0f2d904
Kuba Sztandera <kubuxu@gmail.com>
2014-11-07 15:21:29 +0100

    Mid work

--- main.lua
+++ main.lua
@@ -4,11 +4,29 @@ local sleep = require "socket".sleep
 
 local c = irc.new{nick = "ThatBot"}
 
+pcall(require,"lfs") 
+if not lfs then
+  lfs = {}
+  lfs.dir = function(path)
+    local res = {}
+    return io.popen("dir \"".. path .."\" /b /a"):lines()
+  end
+end
 
-c:connect("irc.esper.net")
-c:join("#Starchasers")
+local modsdir = "./mods"
+package.path = ("lualibs/?.lua;lualibs/?/?.lua;lualibs/?/init.lua;lualibs/?/?/?.lua;lualibs/?/?/init.lua;"):gsub("lualibs","./mods") .. package.path
+for mod in lfs.dir("mods") do
+  mod = mod:gsub("%.lua$","")
+  print(mod)
+  print(pcall(require, mod))
+    
+end
 
-while(true) do
+
+--c:connect("irc.esper.net")
+--c:join("#Starchasers")
+
+while false do
   c:think()
   sleep(0.5)
 end
