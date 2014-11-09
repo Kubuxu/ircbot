ddd237e41314acf986bd6ccb5c2edb766332132b
Jakub Sztandera <kubuxu@gmail.com>
2014-11-09 21:07:46 +0100

    Removed random

--- plugins/calc.lua
+++ plugins/calc.lua
@@ -15,8 +15,8 @@ local function solve(fun,options)
   
   
   local x = {}
-  x[1] = options.guess1 or math.random()
-  x[2] = options.guess2 or math.random()
+  x[1] = options.guess1 or 0.1
+  x[2] = options.guess2 or 0.9
   
   local maxi = options.maxi or 200
   local i = 2
