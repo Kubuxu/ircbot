38714711318243a59783735d0e65c8608397a58b
Kuba Sztandera <kubuxu@gmail.com>
2014-11-12 21:19:04 +0100

    Derp

--- plugins/misc.lua
+++ plugins/misc.lua
@@ -1,6 +1,3 @@
 local hook = require "hook"
 
-
-
-
-hook.new("command_act", function(message, user, channel) hook.irc:sendAction(channel, message) end
+hook.new("command_act", function(message, user, channel) hook.irc:sendAction(channel, message) end)
