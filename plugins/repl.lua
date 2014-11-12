bb56892c98402c30d1c15d12105e5eef7fb60ac8
Kuba Sztandera <kubuxu@gmail.com>
2014-11-12 16:35:07 +0100

    Derp

--- plugins/repl.lua
+++ plugins/repl.lua
@@ -9,9 +9,9 @@ local function handle(message, user, channel)
   if not hook.auth(user) then
     return "Nope"
   end
-  
+ 
   local env = envs[user.nick] or {}
-  
+  hook.irc:sendChat(env.test)
   local message = message:gsub("^%s*=", "return ") 
   
   local ok, result =  pcall(sandbox(message,{[env] = env}))
