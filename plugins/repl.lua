1f5ff504d0b4b382021c035b34c82e6319e00599
Łukasz Magiera <magik6k@users.noreply.github.com>
2014-11-12 17:18:49 +0000

    Added possibly working auto iterator

--- plugins/repl.lua
+++ plugins/repl.lua
@@ -16,6 +16,20 @@ local function handle(message, user, channel)
   local message = message:gsub("^%s*=", "return ") 
   
   local ok, result =  pcall(sandbox(message,{["env"] = env}))
+  
+  if not ok then
+    return result
+  elseif type(result)=="function"then
+    local res = ""
+    for v in result do
+      res = res .. tostring(v) .. ","
+      if res:len() > 1024 then
+        return res
+      end
+    end
+    return res
+  end
+  
   return result
 end
 
