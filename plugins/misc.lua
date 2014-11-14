9772f9f0fc36ee1abd7a5d808ef2cac79f3f585f
Kuba Sztandera <kubuxu@gmail.com>
2014-11-14 17:13:03 +0100

    Added source info

--- plugins/misc.lua
+++ plugins/misc.lua
@@ -3,3 +3,5 @@ local hook = require "hook"
 hook.new("command_act", function(message, user, channel) hook.irc:sendAction(channel, message) end)
 
 hook.new("command_say", function(message, user, channel) hook.irc:sendChat(channel, message) end)
+
+hook.new({"command_source","command_souce"}, function(message, user, channel) return "https://github.com/Kubuxu/ircbot" end)
