203f008559d6c4985276d636f1eb33dfa576dc43
Kuba Sztandera <kubuxu@gmail.com>
2014-11-15 21:19:29 +0100

    Deep copy. Fix

--- utils.lua
+++ utils.lua
@@ -1,15 +1,12 @@
-local utils = {}
-
-function utils.copy(obj, seen)
+local function copy(obj, seen)
   if type(obj) ~= 'table' then return obj end
   if seen and seen[obj] then return seen[obj] end
   local s = seen or {}
   local res = setmetatable({}, getmetatable(obj))
   s[obj] = res
-  for k, v in pairs(obj) do res[copy(k, s)] = utils.copy(v, s) end
+  for k, v in pairs(obj) do res[copy(k, s)] = copy(v, s) end
   return res
 end
 
 
-
-return utils
+return {["copy"] = copy}
