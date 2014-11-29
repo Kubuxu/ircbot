e1f160cea5d1837281a912c007fa4bc57011aacb
Kuba Sztandera <kubuxu@gmail.com>
2014-11-29 15:21:17 +0100

    Fixed join and leave

--- plugins/misc.lua
+++ plugins/misc.lua
@@ -6,5 +6,5 @@ hook.new("command_say", function(message, user, channel) hook.irc:sendChat(chann
 
 hook.new({"command_source","command_souce"}, function(message, user, channel) return "https://github.com/Kubuxu/ircbot" end)
 
-hook.new("join", function(message, user, channel) if not hook.auth(user.nick) then return "Nope" else hook.irc:join(message) end end)
-hook.new("leave", function(message, user, channel) if not hook.auth(user.nick) then return "Nope" else hook.irc:leave(message) end end)
+hook.new("command_join", function(message, user, channel) if not hook.auth(user.nick) then return "Nope" else hook.irc:join(message) end end)
+hook.new("command_leave", function(message, user, channel) if not hook.auth(user.nick) then return "Nope" else hook.irc:leave(message) end end)
