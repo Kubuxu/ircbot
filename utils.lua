cf0d67a536a90b2174f1197b6825667aaefe4e4a
Kubuxu <kubuxu@gmail.com>
2015-05-02 21:06:06 +0200

    update util

--- utils.lua
+++ utils.lua
@@ -1,5 +1,8 @@
+requre"selene"
+
+
 local function copy(obj, seen)
-  if type(obj) ~= 'table' then return obj end
+  if ltype(obj) ~= 'table' then return obj end
   if seen and seen[obj] then return seen[obj] end
   local s = seen or {}
   local res = setmetatable({}, getmetatable(obj))
